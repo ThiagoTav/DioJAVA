6dc4fce48863347fca872c47e37af076144f88a0
thiagotavares2002@gmail.com <thiagotavares2002@gmail.com>
2023-03-23 16:14:20 -0300

    desafiojava

---
+++ src/ContaTerminal.java
@@ -0,0 +1,26 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ContaTerminal {
+
+	public static void main(String[] args) throws Exception {
+		
+		Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+		
+		System.out.println("Digite seu nome");
+		String nome = scanner.next();
+		
+		System.out.println("Digite sua agencia");
+		String agencia = scanner.next();
+		
+		System.out.println("Digite o numero da conta");
+		int numero = scanner.nextInt();
+		
+		System.out.println("Digite seu saldo");
+		double saldo = scanner.nextDouble();
+		
+		
+		System.out.println("Olá "+ nome + " obrigado por criar uma conta em nosso banco, sua agência é " + agencia + " conta "+ numero +" e seu saldo R$"+ saldo +" já está disponível para saque. ");  
+	}
+
+}
